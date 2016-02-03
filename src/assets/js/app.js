d14c3d8cde1a68ee2a525c6e1b856e008813fa51
John Ellison <info@johnwebsites.com>
2016-02-03 13:27:52 +0000

    finish adding Bob Breznaks gawa app into RefugeeProjects.com

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -129,7 +129,7 @@ function normalizeHeaders(element) {
   element["solutionStatement"] = element["whatisthisprojectssolution"];
   element["isFundraising"] = coerceToBool(element["doyouknowifthisprojectiscurrentlyfundraising"]);
   element["isContactInformationAvailable"] = coerceToBool(element["doyouknowofanycontactinformationforthisproject"]);
-  element["category"] = element["whichcategorybestdescribesthisproject"];
+  element["category"] = element["whichsinglecategorybestdescribesthisproject"];
   element["tags"] = String(element["whichtagsbestdescribethisproject"]).split(", ");
   element["twitterUrl"] = element["ifthisprojecthasatwitterprofilewhatistheurl"];
   element["isSubOrganization"] = coerceToBool(element["istheprojectapartofabiggerorganization"]);
